c1e03b95aba40b1ff97eb6b23dfe2c21f600469a
Isaac Watt <isaacwatt@icloud.com>
2019-11-15 17:42:11 -0800

    allow binary search to look within specific range of elements

--- algorithms/searching/binary-search.js
+++ algorithms/searching/binary-search.js
@@ -2,16 +2,17 @@
  * searches for a particular element in a given sorted array
  * @param  {Array} elements is an array of elements
  * @param  {Any} target is an element to be searched for in elements
+ * @param  {Number} left lower bound on range to search for target
+ * @param  {Number} right upper bound on range to search for target
  * @return {Number} index of an occurrence of target in elements or -1 if target does not exist in elements
  * 
  * @runtime O(logn)
  * @space O(1)
  * 
 */
-let binarySearch = function(elements, target) {
+let binarySearch = function(elements, target, left = 0, right = elements.length - 1) {
     let n = elements.length;
-    let left = 0; 
-    let right = n - 1; 
+    if (left < 0 || right >= n) throw new Error("left and right indices must be within range of elements");
     
     while (left <= right) {
         let mid = Math.floor( left + ((right - left) / 2) );
